6f299e1642bdbbc31565ddc7aafc83721e18efcf
Yuan Peng <yuan.peng@ukdd.de>
2023-09-07 13:26:30 +0200

    chore: code clean up

--- src/main/java/org/miracum/etl/fhirtoomop/mapper/FhirMapper.java
+++ src/main/java/org/miracum/etl/fhirtoomop/mapper/FhirMapper.java
@@ -1,10 +1,7 @@
 package org.miracum.etl.fhirtoomop.mapper;
 
-import java.util.List;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.miracum.etl.fhirtoomop.model.OmopModelWrapper;
-import org.miracum.etl.fhirtoomop.model.PostProcessMap;
-import org.miracum.etl.fhirtoomop.model.omop.OmopObservation;
 
 /**
  * The interface FhirMapper defines a general map method for each item processor.
@@ -14,20 +11,4 @@ import org.miracum.etl.fhirtoomop.model.omop.OmopObservation;
  */
 public interface FhirMapper<T extends IBaseResource> {
   OmopModelWrapper map(T resource, boolean isDeleted);
-
-  public default void addToObservationList(
-      List<OmopObservation> observationList, OmopObservation observationEntity) {
-    if (observationList.contains(observationEntity)) {
-      return;
-    }
-    observationList.add(observationEntity);
-  }
-
-  public default void addToPostProcessMap(
-      List<PostProcessMap> postProcessMapList, PostProcessMap postProcessMapEntity) {
-    if (postProcessMapList.contains(postProcessMapEntity)) {
-      return;
-    }
-    postProcessMapList.add(postProcessMapEntity);
-  }
 }
